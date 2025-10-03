f205e083691a4846ef542aa981050da6692f6dca
paularodriguezr <paula.rodriguezr@javeriana.edu.co>
2025-10-03 15:45:50 -0500

    inicio

---
+++ Codigo.R
@@ -0,0 +1,23 @@
+##############################################################
+# Taller de Business Analytics - Caso Tour Marketing (Opry) 
+# Objetivo: Explorar la relación entre ventas y publicidad
+# Integrantes del grupo: Juan Sebastian Cardenas - Paula Rodriguez
+##############################################################
+
+# ============================================================
+# 0. Preparación
+# ============================================================
+
+library(readr)    
+library(dplyr)    
+library(ggplot2)   
+library(psych)  
+
+
+# ============================================================
+# 1. Exploración inicial de los datos
+# ============================================================
+
+opry <- read_csv("Opry_data.csv")
+
+
